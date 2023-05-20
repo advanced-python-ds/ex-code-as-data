55eee2485e3abb172fe1b8458a47aa74f6f68b45
hila <hilapazh@gmail.com>
2023-05-20 07:48:00 +0300

    feat: post solutions

---
+++ day2_func_as_arg/solutions.py
@@ -0,0 +1,65 @@
+from typing import Dict, Any
+
+import numpy as np
+from pprint import pprint
+
+FUNCS_NAMES = [
+    "all",
+    "alltrue",
+    "amax",
+    "amin",
+    "any",
+    "argmax",
+    "argmin",
+    "argsort",
+    "around",
+    "cumprod",
+    "cumproduct",
+    "cumsum",
+    "mean",
+    "ndim",
+    "nonzero",
+    "prod",
+    "product",
+    "ptp",
+    "ravel",
+    "round_",
+    "shape",
+    "size",
+    "sometrue",
+    "sort",
+    "squeeze",
+    "std",
+    "sum",
+    "transpose",
+    "var",
+]
+
+
+def calculate(
+    numbers: iter,
+    *funcs: callable,
+) -> Dict[str, Any]:
+    """
+    Calculate and store the results for a given range of numbers
+    :param numbers: A range of numbers to execute the function on
+    :param funcs: Functions to execute on the numbers
+    :return: A dictionary with keys as function names and values as the results of the function
+    """
+    return {func.__name__: func(numbers) for func in funcs}
+
+
+def main():
+    """
+    Given 29 numpy functions, calculate the results for the first 200 natural numbers
+    :return: A dictionary with keys as function names and values as the results of the function
+    """
+    numbers = np.arange(1, 201)
+    functions = [getattr(np, func_name) for func_name in FUNCS_NAMES]
+    data = calculate(numbers, *functions)
+    return data
+
+
+if __name__ == "__main__":
+    # For your reference
+    pprint(main())
