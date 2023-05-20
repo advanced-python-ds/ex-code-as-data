98b14c88ff289720d31eb1fc181bb508646b730e
hila <hilapazh@gmail.com>
2023-05-20 09:22:43 +0300

    feat: lecture code

---
+++ day2_func_as_arg/l2_func_of_func.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def func_of_func(func: callable) -> None:
+    print(func.__module__)
+    print(func.__name__)
+    print(func(range(11)))
+
+
+
+if __name__ == '__main__':
+    func_of_func(np.sum)
